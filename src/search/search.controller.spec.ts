f65b62c8d6a4f83c82f0aa7961665e60e5a3fc87
HEESOO YANG <didgmltn7769@naver.com>
2022-02-25 00:59:39 +0900

    [ADD] write unit test code

--- src/search/search.controller.spec.ts
+++ src/search/search.controller.spec.ts
@@ -1,20 +1,59 @@
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { UnauthorizedException } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { of } from 'rxjs';
 import { SearchController } from './search.controller';
 import { SearchService } from './search.service';
 
 describe('SearchController', () => {
-  let controller: SearchController;
+  let searchController: SearchController;
+  let httpService: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, ConfigModule],
       controllers: [SearchController],
       providers: [SearchService],
     }).compile();
 
-    controller = module.get<SearchController>(SearchController);
+    searchController = module.get<SearchController>(SearchController);
+    httpService = module.get<HttpService>(HttpService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findStore', () => {
+    it('should return an store info when key is correct', async () => {
+      const data = {
+        data: {
+          documents: [
+            {
+              address_name: '서울특별시 강남구 삼성동',
+              distance: 0,
+              id: '1',
+              place_name: '삼성제과의원',
+              road_address_name: '삼성로',
+              x: '127.03788',
+              y: '37.502424',
+            },
+          ],
+        },
+      };
+      httpService.get = jest.fn().mockImplementationOnce(() => {
+        return of(data);
+      });
+      expect(await searchController.findStore('꿉당')).toStrictEqual({
+        data: data.data.documents,
+        keyword: '꿉당',
+      });
+    });
+
+    it('should return error when key is wrong', async () => {
+      try {
+        await searchController.findStore('꿉당');
+      } catch (err) {
+        expect(err).toBeInstanceOf(UnauthorizedException);
+        expect(err.message).toBe('유효하지 않은 key입니다.');
+      }
+    });
   });
 });
--- src/search/search.service.spec.ts
+++ src/search/search.service.spec.ts
@@ -1,18 +1,57 @@
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { UnauthorizedException } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { of } from 'rxjs';
 import { SearchService } from './search.service';
 
 describe('SearchService', () => {
   let service: SearchService;
+  let httpService: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, ConfigModule],
       providers: [SearchService],
     }).compile();
 
     service = module.get<SearchService>(SearchService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(httpService).toBeDefined();
+  });
+
+  it('should be return data if key is correct', async () => {
+    const data = {
+      data: {
+        documents: [
+          {
+            address_name: '서울특별시 강남구 삼성동',
+            distance: 0,
+            id: '1',
+            place_name: '삼성제과의원',
+            road_address_name: '삼성로',
+            x: '127.03788',
+            y: '37.502424',
+          },
+        ],
+      },
+    };
+    httpService.get = jest.fn().mockImplementationOnce(() => {
+      return of(data);
+    });
+    expect(await service.findStoreInfo('꿉당')).toStrictEqual(data);
+  });
+
+  it('should be return error if key is wrong', async () => {
+    try {
+      await service.findStoreInfo('꿉당');
+    } catch (err) {
+      expect(err).toBeInstanceOf(UnauthorizedException);
+      expect(err.message).toBe('유효하지 않은 key입니다.');
+    }
   });
 });
