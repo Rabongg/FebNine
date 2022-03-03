96c2934c0c5d0d62706a6773afa48f1040810a61
HEESOO YANG <didgmltn7769@naver.com>
2022-03-03 15:07:39 +0900

    [ADD] admin page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get, Render, Version } from '@nestjs/common';
+import { Controller, Get, Query, Render, Version } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Render('food')
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getFoodPage(): string {
+    return;
+  }
+
+  @Render('admin')
+  @Get('admin')
+  getAdminPage(
+    @Query('place') place: string,
+    @Query('address') address: string,
+    @Query('url') url: string,
+  ) {
+    console.log(place, address, url);
+    return { place, address, url };
   }
 }
