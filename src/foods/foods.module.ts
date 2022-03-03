e1e0cfb98676c6c49dc759dbeda363fff5741296
HEESOO YANG <didgmltn7769@naver.com>
2022-03-03 15:09:39 +0900

    [ADD] food-image in fod module

--- src/foods/foods.module.ts
+++ src/foods/foods.module.ts
@@ -4,9 +4,10 @@ import { FoodsController } from './foods.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Food } from './entities/food.entity';
 import { FoodCategory } from './entities/food-category.entity';
+import { FoodImage } from './entities/food-image.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Food, FoodCategory])],
+  imports: [TypeOrmModule.forFeature([Food, FoodImage, FoodCategory])],
   controllers: [FoodsController],
   providers: [FoodsService],
 })
